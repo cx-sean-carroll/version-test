3bdccbc1858f2bb1820c5ee80b4b27a7e77109fe
CxSeanC <81707723+cx-sean-carroll@users.noreply.github.com>
2025-07-15 17:22:17 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js (ES Module syntax)
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from version v1.0.0');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
